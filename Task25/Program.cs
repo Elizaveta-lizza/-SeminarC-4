08cd481692e2953e97f953856a904585e3ea400d
Liza <kuznecovaliza11@yandex.ru>
2023-02-23 03:00:57 +0700

    Задача 25

---
+++ Task25/Program.cs
@@ -0,0 +1,39 @@
+﻿int Getincomingdata(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+bool CheckPower(int Power)
+{
+    if (Power > 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+}
+
+int GetPowValue(int Value, int Power)
+{
+    int Sum = 1;
+    for (int i = 0; i < Power; i++)
+    {
+        Sum = Sum * Value;
+    }
+    return Sum;
+}
+
+int Value = Getincomingdata("Введите число");
+int Power = Getincomingdata("Введите степень");
+if (CheckPower(Power) == true)
+{
+Console.WriteLine(GetPowValue(Value,Power));
+}
+else
+{
+    Console.WriteLine("Степень может быть только положительной");
+}
