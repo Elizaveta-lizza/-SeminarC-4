f376a42245b6dfc381e17b15c0775c068f8169b1
Liza <kuznecovaliza11@yandex.ru>
2023-02-24 22:32:06 +0700

    Задача 27

---
+++ Task27/Program.cs
@@ -0,0 +1,22 @@
+﻿int GetIncomingData(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int GetSum(int Value)
+{
+    int Sum = 0;
+    int Numc;
+    int ValueTMP = Value;
+    while (ValueTMP > 0)
+    {
+        Numc = ValueTMP % 10;
+        Sum = Sum + Numc;
+        ValueTMP = ValueTMP / 10;
+    }
+    return Sum;
+}
+
+int Value = GetIncomingData("Введите число");
+
+Console.WriteLine(GetSum(Value));
